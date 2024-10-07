6ed88586409255d0455243ca12fb8747db1032e9
Pobre01 <163008650+Pobre01@users.noreply.github.com>
2024-10-07 11:23:30 -0300

    .

---
+++ app.js
@@ -0,0 +1,41 @@
+criaCartao(
+    'Programação 2.0',
+    'O que é phyton',
+    'O phyton é uma linguagem de programação',
+)
+
+criaCartao(
+    'Dinossauro',
+    'Em que ano surgiu o dinossauro',
+    'De 208 a 245 milhões de anos atrás',
+)
+
+criaCartao(
+    'Cobra',
+    'Em que ano surgiu a cobra',
+    'Há 128 milhões de anos',
+)
+
+criaCartao(
+    'Macaco',
+    'Em que ano o macaco surgiu',
+    'Cerca de 65 milhões de anos'
+)
+
+criaCartao(
+    'Jacaré',
+    'Em que ano surgiu o jacaré',
+    'Surgiu em nosso planeta há pelo menos 200 milhões de anos.'
+)
+
+criaCartao(
+    'Furão',
+    'Em que ano surgiu o furão',
+    'Ele surgiu a cerca de 2.500 anos'
+)
+
+criaCartao(
+    'Avestruz',
+    'Em que ano surgiu o avestruz',
+    'Ele surgiu a cerca de 12  milhões de anos atrás'
+)
